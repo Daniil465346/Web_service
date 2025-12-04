378f5140faeb648bf62db347bf57111a0a5c10eb
Daniil465346 <dkuzminov@sfedu.ru>
2025-12-04 18:19:34 +0300

    Update Security.cs

--- Models/Security.cs
+++ Models/Security.cs
@@ -1,10 +1,15 @@
-﻿namespace InvestmentApi.Models
+namespace InvestmentApi.Models
 {
     public class Security
     {
         public int Id { get; set; }
         public string Ticker { get; set; } 
         public string Name { get; set; }   
-        public decimal CurrentPrice { get; set; } 
+        public decimal CurrentPrice { get; set; }
+
+        public decimal BasePrice { get; set; }
+        public decimal MinPrice { get; set; }
+        public decimal MaxPrice { get; set; }
+        public decimal PriceChangeRange { get; set; }
     }
 }
